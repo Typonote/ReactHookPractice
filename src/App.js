34cce2e350bc626a01ff03748b441201add5c3cd
Typonote <kimradium@naver.com>
2021-11-06 12:57:39 +0900

    #5 useReducer 카운터 구현 (p.200)

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import Info from "./HookPractice/Info";
 function App() {
   const [visible, setVisible] = useState(false);
   return (
-    <div className="text-center p-5 ">
-      <button
-        className="p-2 pl-5 pr-5 bg-transparent border-2 border-indigo-500 text-indigo-500 text-lg rounded-lg hover:bg-indigo-500 hover:text-gray-100 focus:border-4 focus:border-indigo-300 mb-5"
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
+    // <div className="text-center p-5 ">
+    //   <button
+    //     className="p-2 pl-5 pr-5 bg-transparent border-2 border-indigo-500 text-indigo-500 text-lg rounded-lg hover:bg-indigo-500 hover:text-gray-100 focus:border-4 focus:border-indigo-300 mb-5"
+    //     onClick={() => {
+    //       setVisible(!visible);
+    //     }}
+    //   >
+    //     {visible ? "숨기기" : "보이기"}
+    //   </button>
+    //   <hr />
+    //   {visible && <Info />}
+    // </div>
+    <Counter />
   );
 }
 
--- src/HookPractice/Counter.js
+++ src/HookPractice/Counter.js
@@ -1,25 +1,43 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+
+// useReducer는 더 다양한 컴포넌트 상황에서 다양한 상태를 다름 값을 업데이트 할때 사용하는 hook
+// 현재 상태와 필요한 정보를 담은 액션값을 전달 받아, 새로운 상태를 반환한다.
+function reducer(state, action) {
+  // action.type 에 따라 다른 작업 수행
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 };
+    case "DECREMENT":
+      return { value: state.value - 1 };
+    default:
+      // 아무것도 해당되지 않을 때 기존 상태 반환
+      return state;
+  }
+}
 
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  // useReducer(리듀서 함수,{기본값}) => state(현재 상태)값과 dispatch(액션 발생 함수)을 받음.
+
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
 
   return (
     <div className="flex flex-col text-center mt-10 items-center justify-center py-5 ">
       <p className="text-lg font-bold">
-        현재 카운터 값은 <b>{value}</b> 입니다.
+        현재 카운터 값은 <b>{state.value}</b> 입니다.
       </p>
 
       <div className="m-5">
         <button
           className="py-3 px-6 text-white rounded-lg bg-green-400 shadow-lg inline-block m-1"
-          onClick={() => setValue(value + 1)}
+          // 함수 안에 액션 값을 넣으면 리듀서 함수가 호출 됨.
+          onClick={() => dispatch({ type: "INCREMENT" })}
         >
           +1
         </button>
 
         <button
           className="py-3 px-6 text-white rounded-lg bg-red-500 shadow-lg inline-block m-1"
-          onClick={() => setValue(value - 1)}
+          onClick={() => dispatch({ type: "INCREMENT" })}
         >
           -1
         </button>
